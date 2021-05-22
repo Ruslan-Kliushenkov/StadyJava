cb1f9c041b0a32e6c65dbe7b2722f7948335d9a3
Ruslan-Kliushenkov <ruslan.klyshenkov@gmail.com>
2021-05-22 13:48:51 +0300

    Modul - 1, Task - 1

--- src/ua/kliushenkov/modul1/Task1/TaskOne.java
+++ src/ua/kliushenkov/modul1/Task1/TaskOne.java
@@ -1,4 +1,30 @@
 package ua.kliushenkov.modul1.Task1;
 
 public class TaskOne {
+    public static void main(String[] args) {
+        int[] array = {1, 1, 3, 3, 5, 5, 7, 7, 8, 8, 0, 0, -1, -1, -1, 0};
+        System.out.println(calcUniNum(array));
+    }
+
+    protected static int calcUniNum(int[] mass) {
+        int calc = 0;
+        int zeroCheck = 0;
+        for (int k : mass) {
+            if (k == 0) {
+                calc++;
+                zeroCheck = 1;
+            }
+        }
+        for (int i = 0; i < mass.length; i++) {
+            for (int j = i + 1; j < mass.length; j++) {
+                if (mass[i] != 0) {
+                    if (mass[i] == mass[j] && mass[j] != 0) {
+                        calc++;
+                        mass[j] = 0;
+                    }
+                }
+            }
+        }
+        return mass.length - calc + zeroCheck;
+    }
 }
